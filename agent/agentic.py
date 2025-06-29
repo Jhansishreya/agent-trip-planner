92e5371a68cbfb2cad1a3d9e5c6e6836ce4cee60
shreya <shreyajhansi3999@gmail.com>
2025-06-29 13:21:34 +0530

    agent

---
+++ agent/agentic.py
@@ -0,0 +1,19 @@
+from utils.model_loader import ModelLoader
+from prompt_library.prompt import SYSTEM_PROMPT
+from langgraph.graph import StateGraph,MessagesState,END,START
+from langgraph.prebuilt import ToolNode,tools_condition
+
+
+class GraphBuldier():
+    def __init__(self):
+        pass
+    def agent_function(self):
+        pass
+    def build_graph(self):
+        """
+        Build a graph based on the provided data.
+        """
+        # Placeholder for graph building logic
+        pass
+    def __call__(self):
+        pass
